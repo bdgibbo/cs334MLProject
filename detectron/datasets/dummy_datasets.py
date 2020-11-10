bae19bc919c428eac66e2d1b1eb6b963bbf029d8
bdgibbo <65683195+bdgibbo@users.noreply.github.com>
2020-11-10 01:07:01 -0500

    Add files via upload

---
+++ detectron/datasets/dummy_datasets.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##############################################################################
+"""Provide stub objects that can act as stand-in "dummy" datasets for simple use
+cases, like getting all classes in a dataset. This exists so that demos can be
+run without requiring users to download/install datasets first.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from detectron.utils.collections import AttrDict
+
+
+def get_coco_dataset():
+    """A dummy COCO dataset that includes only the 'classes' field."""
+    ds = AttrDict()
+    classes = [
+        '__background__', 'gun'
+    ]
+    ds.classes = {i: name for i, name in enumerate(classes)}
+    return ds
