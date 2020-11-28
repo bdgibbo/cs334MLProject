52d8587be217cf13061689d4d12e804ce5a0ccfc
Ubuntu <detection@GunDetection.xo1coghx0souhoejphstc21e0f.bx.internal.cloudapp.net>
2020-11-28 18:12:39 +0000

    removing scripts

--- scripts/download_models.sh
+++
@@ -1,4 +0,0 @@
-cd detectron/datasets/data
-python ../../../tools/download.py 1YO_HP87RicnCHRCB1BzOY3WjXQQm7mUP trained_weights.tar
-tar -xzf trained_weights.tar
-rm -rf trained_weights.tar
--- scripts/download_train_data.sh
+++
@@ -1,5 +0,0 @@
-./scripts/download_test_data.sh
-cd detectron/datasets/data
-python ../../../tools/download.py 1bDWa-iAOWY3opnyjRxWn6rLOtLdWTz4Q train.tar
-tar -xzf train.tar -C gun
-rm -rf train.tar
